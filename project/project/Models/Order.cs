04be577b86c47bf05093ce7325d6e0bea1e6101c
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-03-21 16:56:20 +0700

    add models, controllers, views

---
+++ project/project/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        [Range(18, 2)]
+        public decimal TotalAmount { get; set; }
+        [StringLength(50, ErrorMessage = "Status cannot exceed 50 characters")]
+        public string? Status { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ project/project/Models/ProductImage.cs
@@ -0,0 +1,10 @@
+﻿namespace project.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string? Url { get; set; }
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+    }
+}

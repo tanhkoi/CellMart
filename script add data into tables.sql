4cf6cd90d06acf124ad9619ad33916533037d58a
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-03-28 19:21:39 +0700

    update

--- script add data into tables.sql
+++ script add data into tables.sql
@@ -68,3 +68,53 @@ Cấu hình iPhone 14 Pro Max mạnh mẽ, hiệu năng cực khủng từ chips
 Làm chủ công nghệ nhiếp ảnh - Camera sau 48MP, cảm biến TOF sống động
 Pin liền lithium-ion kết hợp cùng công nghệ sạc nhanh cải tiến', 28490000.00, N'~\images\Apple\iPhone 14 Pro Max 256GB.png', 1, 0);
 --oppo
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO A17K', N'Chuẩn nét từng khung hình - Trang bị màn hình LCD 6.56 inch với độ phân giải HD+
+Hiệu năng cải tiến, bứt phá tốc độ - Chip Helio G35, RAM 4GB và bộ nhớ trong 64GB
+Trọn vẹn năng lượng suốt cả ngày - Viên pin lớn 5000 mAh, sạc 10W
+Bảo mật nâng cao với mở khoá vân tay cạnh viền, mở khoá khuôn mặt', 2790000.00, N'~\images\OPPO\OPPO A17K.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO A18 4GB 128GB', N'Hiệu năng vượt trội với chip Helio G85 - Giúp bạn xử lý các tác vụ mượt mà không cần phải lo lắng có bị giật, lag.
+Độ phân giải HD+ sặc nét cùng tần số quét 90Hz, mang lại những hình ảnh sống động và mượt mà nhất.
+Dung lượng pin 5000 mAh mạnh mẽ - Đảm bảo bạn sẽ luôn kết nối và sử dụng trong nhiều giờ liền.
+Thiết kế thời trang và hiện đại với các cạnh viền được vát phẳng và bo tròn ở các góc.', 3690000.00, N'~\images\OPPO\OPPO A18 4GB 128GB.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO A77s 8GB 128GB', N'Nâng tầm trải nghiệm thị giác - Tấm nền IPS LCD với kích thước 6.56 inch, tần số quét 90Hz
+Năng lượng tiếp sức cho cả ngày dài - 5000 mAh, sạc siêu nhanh SuperVOOC 33 W
+Long lanh từ trong ra ngoài với thiết kế OPPO Glow, mặt lưng hoàn thiện từ thủy tinh hữu cơ
+Trải nghiệm ổn định mọi tác vụ - Chip Snapdragon 680 8, RAM 8 GB và khả năng mở rộng RAM', 5190000.00, N'~\images\OPPO\OPPO A77s 8GB 128GB.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO A57 4GB 128GB', N'Không gian hiển thị chất lượng - Màn hình IPS LCD 6.56 inches sắc nét
+Cấu hình ổn định, thách thức mọi tác vụ - MediaTek Helio G35
+Camera chụp ảnh chuyên nghiệp - Cụm camera 13 MP, đa dạng chế độ và filter
+Năng lượng bất tận - Dung lượng pin 5000 mAh, sạc nhanh 33W', 2590000.00, N'~\images\OPPO\OPPO A57 4GB 128GB.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'Oppo A74 8GB 128GB', N'Không gian hiển thị chất lượng - Màn hình IPS LCD 6.56 inches sắc nét
+Cấu hình ổn định, thách thức mọi tác vụ - MediaTek Helio G35
+Camera chụp ảnh chuyên nghiệp - Cụm camera 13 MP, đa dạng chế độ và filter
+Năng lượng bất tận - Dung lượng pin 5000 mAh, sạc nhanh 33W', 3390000.00, N'~\images\OPPO\Oppo A74 8GB 128GB.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO Find N2', N'OPPO Find N2 trang bị hàng loạt thông số cấu hình cao như chip Snapdragon 8+ Gen 1, kết hợp 12GB RAM, tấm nền AMOLED, kích thước màn hình chính 7.1 inch, camera lên tới 50MP, dung lượng pin 4.520 mAh, sạc nhanh 67W.', 9999999999.00, N'~\images\OPPO\OPPO Find N2.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO Find N2 Flip 8Gb 256GB', N'Thiết kế bền bỉ cho phép gập đến 400.000 lần, giúp dễ dàng gấp gọn và mang theo
+Màn hình phụ kích thước 3.26 inch cho khả năng hiển thị trực quan và nhiều chi tiết
+Hệ thống ống kính chất lượng cao với cảm biến Sony IMX890 có độ phân giải 50MP
+Hiệu năng mạnh mẽ với vi xử lý MediaTek 9000+ kết hợp RAM 8GB và ROM 256GB', 16990000.00, N'~\images\OPPO\OPPO Find N2 Flip 8Gb 256GB.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO Reno7 5G (8GB 256GB)', N'Trải nghiệm mọi tác vụ mượt mà - Chip MediaTek Dimensity 900 5G mạnh mẽ, RAM khủng 8 GB
+Ghi lại những câu chuyện sống động màu sắc - Camera chính 64MP, camera selfie độ phân giải cao
+Năng lượng bền bỉ cho cả ngày dài - Viên pin lớn 4500 mAh cùng sạc nhanh 65 W
+Màn hình giải trí bất tận - Màn hình AMOLED 6.4"", tần số quét 90 Hz cho hình ảnh sống động, sắc nét', 7590000.00, N'~\images\OPPO\OPPO Reno7 5G (8GB 256GB).png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO Reno 7 Pro', N'Đã mắt hơn với màn hình chất lượng - Màn hình AMOLED 6.5 inch cực lớn, hỗ trợ HDR 10+
+Chiến game mượt mà - chip MediaTek Dimensity 1200 Max 8 nhân mạnh mẽ, RAM lớn đến 12GB
+Chụp hình chuyên nghiệp hơn - Camera chính 50 MP, camera góc rộng 8 MP và camera macro 2 MP
+Sạc nhanh trong tích tắc - Dung lượng 4500 mAh, công nghệ sạc SuperVOOC 2.0 có công suất tối đa 65W', 9990000.00, N'~\images\OPPO\OPPO Reno 7 Pro.png', 2, 0);
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'OPPO Reno11 F 5G 8GB 256GB', N'OPPO Reno11 F 5G cung cấp trải nghiệm hiển thị, xử lý siêu mượt mà với màn hình AMOLED 6.7 inch hiện đại cùng chipset MediaTek Dimensity 7050 mạnh mẽ. Hệ thống quay chụp trên thế hệ Reno11 F 5G này được cải thiện hơn thông qua cụm 3 camera với độ phân giải lần lượt là 64MP, 8MP và 2MP. Ngoài ra, cung cấp năng lượng cho thế hệ OPPO smartphone này là viên pin 5000mAh cùng sạc nhanh 67W, mang tới trải nghiệm liền mạch suốt ngày dài.',  8990000.00, N'~\images\OPPO\OPPO Reno11 F 5G 8GB 256GB.png', 2, 0);
+--samsung
+INSERT INTO Product (Name, Description, Price, ImageUrl, CategoryId, IsDeleted)
+VALUES (N'Samsung Galaxy S24 Ultra 12GB 256GB', N'Mở khoá giới hạn tiềm năng với AI - Hỗ trợ phiên dịch cuộc gọi, khoanh vùng tìm kiếm, Trợ lí Note và chình sửa anh
+Tuyệt tác thiết kế bền bỉ và hoàn hảo - Vỏ ngoài bằng titan mới cùng màu sắc lấy cảm hứng từ chất liệu đá tự nhiên
+Tích hợp S-Pen cực nhạy - Thoải mát viết, chạm thật chính xác trên màn hình cùng nhiều tính năng tiện ích
+Nắm trong tay trọn bộ chi tiết chân thực nhất - Camera 200MP hỗ trợ khả năng xử lý AI cải thiện độ nét và tông màu', 28690000.00, N'~\images\Samsung\Samsung Galaxy S24 Ultra 12GB 256GB.webp', 2, 0);

684f594f8f035e28b77569e78faca6019f4addb2
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-04-04 16:42:45 +0700

    Fix bug can't register

---
+++ project/project/Migrations/20240404094131_extendIdentity.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project.Migrations
+{
+    /// <inheritdoc />
+    public partial class extendIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_User_UserId",
+                table: "Cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Order_UserId",
+                table: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cart_UserId",
+                table: "Cart");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Order",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Cart",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_UserId1",
+                table: "Order",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_UserId1",
+                table: "Cart",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_AspNetUsers_UserId1",
+                table: "Cart",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_AspNetUsers_UserId1",
+                table: "Order",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_AspNetUsers_UserId1",
+                table: "Cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_AspNetUsers_UserId1",
+                table: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Order_UserId1",
+                table: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cart_UserId1",
+                table: "Cart");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Cart");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_UserId",
+                table: "Order",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_UserId",
+                table: "Cart",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_User_UserId",
+                table: "Cart",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- project/project/Models/User.cs
+++ project/project/Models/User.cs
@@ -6,24 +6,13 @@ namespace project.Models
     public class User : IdentityUser
     {
         [Required]
-        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string? Address { get; set; }
+        public bool IsDeleted { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
 
-        [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; }
-
-        //[Required(ErrorMessage = "Password is required")]
-        //public string Password { get; set; }
-
-        [Required(ErrorMessage = "Email is required")]
-        public string Email { get; set; }
-        //public string Phone { get; set; }
-        //public string Address { get; set; }
-        //public bool IsDeleted { get; set; }
-
-        //[DataType(DataType.Date)]
-        //public DateTime CreatedAt { get; set; }
-
-        //[DataType(DataType.Date)]
-        //public DateTime UpdatedAt { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime UpdatedAt { get; set; }
     }
 }

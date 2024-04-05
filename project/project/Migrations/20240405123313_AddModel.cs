ea9997d739e46ed78d9bed931b67f41d3ba78b43
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-04-05 20:33:00 +0700

    update

---
+++ project/project/Migrations/20240405123313_AddModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
